5c35a75588bc7b37561da42660f4bc42c196acca
Erasmo Carreon <c4rreon1234@gmail.com>
2022-05-13 09:34:48 -0500

    Arreglando fechas

--- src/api/components/appointment/network.js
+++ src/api/components/appointment/network.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const {
   createAppointmentSchema,
@@ -6,157 +6,178 @@ const {
   appointmentIdSchema,
   citaIdSchema,
   dateSchema,
-  dateUpdateSchema
-} = require('../../../utils/validations/schemas/appointment.schema'); // eslint-disable-line
-const validationHandler = require('../../../utils/middlewares/validationHandler');
-const controller = require('./controller');
-const checkJwt = require('../../../utils/middlewares/auth/checkJwt');
-const checkRole = require('../../../utils/middlewares/auth/checkRole');
+  dateUpdateSchema,
+} = require("../../../utils/validations/schemas/appointment.schema"); // eslint-disable-line
+const validationHandler = require("../../../utils/middlewares/validationHandler");
+const controller = require("./controller");
+const checkJwt = require("../../../utils/middlewares/auth/checkJwt");
+const checkRole = require("../../../utils/middlewares/auth/checkRole");
 const { isLoggedIn } = require("../../../ssr/lib/auth");
 
-router.get('/', isLoggedIn, (req, res, next) => {
+router.get("/", isLoggedIn, (req, res, next) => {
   try {
-    res.render('doctors/menu');
+    res.render("doctors/menu");
   } catch (error) {
     next(error);
   }
 });
 
-router.get('/add', isLoggedIn, (req, res, next) => {
+router.get("/add", isLoggedIn, (req, res, next) => {
   try {
-    res.render('appointment/add');
+    res.render("appointment/add");
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/add', isLoggedIn, validationHandler(createAppointmentSchema), async (req, res, next) => {
-  const {
-    date,
-    idDoctor,
-    idPatient,
-  } = req.body;
-  const newAppointment = {
-    date,
-    idDoctor,
-    idPatient
-  };
+router.post(
+  "/add",
+  isLoggedIn,
+  validationHandler(createAppointmentSchema),
+  async (req, res, next) => {
+    const { date, idDoctor, idPatient } = req.body;
+    const newAppointment = {
+      date,
+      idDoctor,
+      idPatient,
+    };
 
-  try {
-    const appointments = await controller.appointmentCreation(newAppointment);
-    if (appointments) {
-      const appointment = appointments.dataValues;
-      console.log(appointment);
-      res.render('appointment/listaddGet', { appointment });
+    try {
+      const exist = await controller.getCitaByDate(newAppointment.date);
+      if (!exist) {
+        const appointments = await controller.appointmentCreation(
+          newAppointment
+        );
+        if (appointments) {
+          const appointment = appointments.dataValues;
+          console.log(appointment);
+          res.render("appointment/listaddGet", { appointment });
+        }
+      } else {
+        res.render("home/errores");
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-}
 );
 
-router.get('/update', isLoggedIn, (req, res, next) => {
+router.get("/update", isLoggedIn, (req, res, next) => {
   try {
-    res.render('appointment/update');
+    res.render("appointment/update");
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/update', isLoggedIn, validationHandler(updateAppointmentSchema), async (req, res, next) => {
-  const {
-    id,
-    date,
-    idPatient
-  } = req.body;
-  const updateAppointment = {
-    id,
-    date,
-    idPatient
-  };
-  try {
-    const cita = await controller.appointmentUpdate(updateAppointment);
-    res.render('appointment/update');
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/list', isLoggedIn, async (req, res, next) => {
-  try {
-    res.render('appointment/list');
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/list/id', async (req, res, next) => {
-  try {
-    res.render('appointment/listId');
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post('/list/id', isLoggedIn, validationHandler({ id: appointmentIdSchema }), async (req, res, next) => {
-  const { id } = req.body;
-
-  try {
-    const appointments = await controller.getCitaById(id);
-    if (appointments) {
-      const appointment = appointments.dataValues;
-      console.log(appointment);
-      res.render('appointment/listIdGet', { appointment });
-    } else {
-      res.render('appointment/dontExist');
+router.post(
+  "/update",
+  isLoggedIn,
+  validationHandler(updateAppointmentSchema),
+  async (req, res, next) => {
+    const { id, date, idPatient } = req.body;
+    const updateAppointment = {
+      id,
+      date,
+      idPatient,
+    };
+    try {
+      const cita = await controller.appointmentUpdate(updateAppointment);
+      res.render("appointment/update");
+    } catch (error) {
+      next(error);
     }
+  }
+);
+
+router.get("/list", isLoggedIn, async (req, res, next) => {
+  try {
+    res.render("appointment/list");
   } catch (error) {
     next(error);
   }
 });
 
-router.get('/list/date', isLoggedIn, async (req, res, next) => {
+router.get("/list/id", async (req, res, next) => {
   try {
-    res.render('appointment/listDate');
+    res.render("appointment/listId");
   } catch (error) {
     next(error);
   }
 });
 
-router.post('/list/date', isLoggedIn, validationHandler({ date: dateSchema }), async (req, res, next) => {
-  const { date } = req.body;
+router.post(
+  "/list/id",
+  isLoggedIn,
+  validationHandler({ id: appointmentIdSchema }),
+  async (req, res, next) => {
+    const { id } = req.body;
 
-  try {
-    const appointments = await controller.getCitaByDate(date);
-    if (appointments) {
-      const appointment = appointments.dataValues;
-      console.log(appointment);
-      res.render('appointment/listDateGet', { appointment });
-    } else {
-      res.render('appointment/dontExist');
+    try {
+      const appointments = await controller.getCitaById(id);
+      if (appointments) {
+        const appointment = appointments.dataValues;
+        console.log(appointment);
+        res.render("appointment/listIdGet", { appointment });
+      } else {
+        res.render("appointment/dontExist");
+      }
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error);
   }
-});
+);
 
-
-router.post('/list/up', isLoggedIn, validationHandler({ date: dateSchema }), async (req, res, next) => {
-  const { date } = req.body;
-
+router.get("/list/date", isLoggedIn, async (req, res, next) => {
   try {
-    const appointments = await controller.getCitaByDate(date);
-    if (appointments) {
-      const appointment = appointments.dataValues;
-      console.log(appointment);
-      res.render('appointment/listUpdateGet', { appointment });
-    } else {
-      res.render('appointment/dontExist');
-    }
+    res.render("appointment/listDate");
   } catch (error) {
     next(error);
   }
 });
 
+router.post(
+  "/list/date",
+  isLoggedIn,
+  validationHandler({ date: dateSchema }),
+  async (req, res, next) => {
+    const { date } = req.body;
+
+    try {
+      const appointments = await controller.getCitaByDate(date);
+      if (appointments) {
+        const appointment = appointments.dataValues;
+        console.log(appointment);
+        res.render("appointment/listDateGet", { appointment });
+      } else {
+        res.render("appointment/dontExist");
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  "/list/up",
+  isLoggedIn,
+  validationHandler({ date: dateSchema }),
+  async (req, res, next) => {
+    const { date } = req.body;
+
+    try {
+      const appointments = await controller.getCitaByDate(date);
+      if (appointments) {
+        const appointment = appointments.dataValues;
+        console.log(appointment);
+        res.render("appointment/listUpdateGet", { appointment });
+      } else {
+        res.render("appointment/dontExist");
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
